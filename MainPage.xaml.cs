5ab1b1d7eccb5c27ad71679a030738f729fee3e6
Notv7 <90964382+Notv7@users.noreply.github.com>
2024-02-08 10:40:32 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count--;
+
+            if (count == 11)
+                CounterBtn.Text = $"touched {count} time";
+            else
+                CounterBtn.Text = $"Touched {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
